cc0cf06d49feb22176125efd4b41b90e210927ee
Saptarshi Basu <sapb2004@gmail.com>
2018-10-27 12:31:04 +0530

    code formatted with Lucene code formatter

--- src/test/java/com/sapbasu/javastudy/CryptoTest.java
+++ src/test/java/com/sapbasu/javastudy/CryptoTest.java
@@ -13,35 +13,38 @@ import javax.crypto.spec.SecretKeySpec;
 import org.junit.jupiter.api.Test;
 
 public class CryptoTest {
-	
-	private int KEY_LEN = 256; //bits
-
-	@Test
-	public void whenDecryptCalled_givenEncryptedTest_returnsDecryptedBytes() throws Exception {
-		Crypto crypto = new Crypto();
-		
-		char[] input = { 'e', 'n', 'c', 'r', 'y', 'p', 't', 'i', 'o', 'n' };
-		byte[] inputBytes = convertInputToBytes(input);
-		
-		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-		keyGen.init(KEY_LEN);
-		SecretKey secretKey = keyGen.generateKey();
-		
-		SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
-
-		byte[] encryptedBytes = crypto.encrypt(inputBytes, secretKeySpec);
-		byte[] decryptedBytes = crypto.decrypt(encryptedBytes, secretKeySpec);
-		
-		assertArrayEquals(inputBytes, decryptedBytes);
-		
-	}
-	
-	private byte[] convertInputToBytes(char[] input) {
-		CharBuffer charBuf = CharBuffer.wrap(input);
-		ByteBuffer byteBuf = Charset.forName(Charset.defaultCharset().name()).encode(charBuf);
-		byte[] inputBytes = byteBuf.array();
-		charBuf.clear();
-		byteBuf.clear();
-		return inputBytes;
-	}
+  
+  private int KEY_LEN = 256; // bits
+  
+  @Test
+  public void whenDecryptCalled_givenEncryptedTest_returnsDecryptedBytes()
+      throws Exception {
+    Crypto crypto = new Crypto();
+    
+    char[] input = {'e', 'n', 'c', 'r', 'y', 'p', 't', 'i', 'o', 'n'};
+    byte[] inputBytes = convertInputToBytes(input);
+    
+    KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+    keyGen.init(KEY_LEN);
+    SecretKey secretKey = keyGen.generateKey();
+    
+    SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getEncoded(),
+        "AES");
+    
+    byte[] encryptedBytes = crypto.encrypt(inputBytes, secretKeySpec);
+    byte[] decryptedBytes = crypto.decrypt(encryptedBytes, secretKeySpec);
+    
+    assertArrayEquals(inputBytes, decryptedBytes);
+    
+  }
+  
+  private byte[] convertInputToBytes(char[] input) {
+    CharBuffer charBuf = CharBuffer.wrap(input);
+    ByteBuffer byteBuf = Charset.forName(Charset.defaultCharset().name())
+        .encode(charBuf);
+    byte[] inputBytes = byteBuf.array();
+    charBuf.clear();
+    byteBuf.clear();
+    return inputBytes;
+  }
 }
